804589202079f74ec3237b0135621a8d9669860c
sarek1973255-cloud <sarek1973255@gmail.com>
2026-05-11 01:26:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from telethon import TelegramClient
+import asyncio
 
-pip install telethon
+API_ID = 123456
+API_HASH = "YOUR_API_HASH"
+SESSION_NAME = "session"
+
+client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
+
+async def main():
+    await client.start()
+    print("✅ LOGIN SUCCESSFUL")
+
+    me = await client.get_me()
+    print("User:", me.username)
+
+asyncio.run(main())
